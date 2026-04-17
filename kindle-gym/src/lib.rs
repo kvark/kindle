11a65ae980a3c9853427d84f7ab30f436e643710
Dzmitry Malyshau <kvark@fastmail.com>
2026-04-16 23:02:44 -0700

    Uprev meganeura

--- kindle-gym/src/lib.rs
+++ kindle-gym/src/lib.rs
@@ -2,7 +2,21 @@
 //!
 //! Ported from OpenAI Gymnasium where noted.
 
-#![allow(clippy::new_without_default, clippy::too_many_arguments)]
+#![allow(
+    clippy::match_like_matches_macro,
+    clippy::redundant_pattern_matching,
+    clippy::needless_lifetimes,
+    clippy::new_without_default,
+    clippy::single_match,
+    clippy::too_many_arguments,
+    clippy::collapsible_if,
+    clippy::collapsible_match
+)]
+#![warn(
+    trivial_numeric_casts,
+    unused_extern_crates,
+    clippy::pattern_type_mismatch
+)]
 
 pub mod acrobot;
 pub mod cart_pole;
