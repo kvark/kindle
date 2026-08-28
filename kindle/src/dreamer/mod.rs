878cf859fd3b7ee1e4f7bde908127d1a2e53fa7e
Dzmitry Malyshau <kvark@fastmail.com>
2026-08-28 22:27:32 +0000

    test: keep backend revision metadata in sync

--- kindle/src/dreamer/mod.rs
+++ kindle/src/dreamer/mod.rs
@@ -26,3 +26,33 @@ pub const DREAMERV3_UPSTREAM_REV: &str = "e3f02248693a79dc8b0ebd62c93683888ddacc
 pub const MEGANEURA_REV: &str = "5429fcd043acd58dec77f35a44c452737d892fd2";
 /// Blade revision providing the shared graphics runtime.
 pub const BLADE_REV: &str = "824d55fbd6485a102ee220553bc54a30c1af5936";
+
+#[cfg(test)]
+mod tests {
+    use super::{BLADE_REV, MEGANEURA_REV};
+
+    #[test]
+    fn reported_backend_revisions_match_every_dependency_lock() {
+        const KINDLE_MANIFEST: &str = include_str!("../../Cargo.toml");
+        const WORKSPACE_LOCK: &str = include_str!("../../../Cargo.lock");
+        const PYTHON_LOCK: &str = include_str!("../../../python/Cargo.lock");
+
+        for (repository, revision) in [("meganeura", MEGANEURA_REV), ("blade", BLADE_REV)] {
+            let manifest_pin =
+                format!("git = \"https://github.com/kvark/{repository}\", rev = \"{revision}\"");
+            assert!(
+                KINDLE_MANIFEST.contains(&manifest_pin),
+                "Kindle manifest does not pin reported {repository} revision {revision}"
+            );
+
+            let lock_pin =
+                format!("git+https://github.com/kvark/{repository}?rev={revision}#{revision}");
+            for (name, lock) in [("workspace", WORKSPACE_LOCK), ("Python", PYTHON_LOCK)] {
+                assert!(
+                    lock.contains(&lock_pin),
+                    "{name} lock does not resolve reported {repository} revision {revision}"
+                );
+            }
+        }
+    }
+}
